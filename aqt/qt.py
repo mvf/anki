a62d896b295c3971627250056f058ef19d2ebc77
Matthias von Faber <mvf@gmx.eu>
2017-10-25 19:24:18 +0200

    Remove Qt version check

--- aqt/qt.py
+++ aqt/qt.py
@@ -42,9 +42,6 @@ qtmajor = (QT_VERSION & 0xff0000) >> 16
 qtminor = (QT_VERSION & 0x00ff00) >> 8
 qtpoint = QT_VERSION & 0xff
 
-if qtmajor != 5 or qtminor < 9 or qtminor == 10:
-    raise Exception("Anki does not support your Qt version.")
-
 # GUI code assumes python 3.6+
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     raise Exception("Anki requires Python 3.6+")
